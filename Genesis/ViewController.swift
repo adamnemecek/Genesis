07bfb5ae09bd1ca6f73223eb79e9495ccf93d6c5
Gerard Iglesias <gerard_iglesias@me.com>
2017-08-27 23:39:06 +0200

    Start Genesis Xcode workspace

---
+++ Genesis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Genesis
+//
+//  Created by Gerard Iglesias on 27/08/2017.
+//  Copyright © 2017 Gerard Iglesias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
